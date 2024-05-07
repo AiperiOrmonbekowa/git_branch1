b4669a64e283db718ac8a70776b51a7ace4bcf0f
Aiperi Ormonbekova <aiperi.ormonbekovna@gmail.com>
2024-05-07 19:29:08 +0600

    hjfdbfj

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:git_branch_1/view/home_view.dart';
+import 'package:git_branch_1/features/presention/pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(),
+      home: const HomePage(),
     );
   }
 }
